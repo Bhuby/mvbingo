ca5fc38f761bd9e1e92d7533331c67fdea58f81a
Bhuby <55616111+Bhuby@users.noreply.github.com>
2020-09-22 12:28:55 -0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -2,18 +2,21 @@ var alphabet = "abcdefghijklnopqrstuvwxy";
 var numsUsed = [];
 var rand = 0;
 for(var j=0; j<189; j++){
-    //var domain = "https://bhuby.github.io/mvbingo/index.html?";
-    var domain = "file:///Users/bhuvanb.balagar/bingo/index.html?";
-    for(var i=0; i<alphabet.length; i++){
-    	//console.log(numsUsed.includes(rand));
-    	while(numsUsed.includes(rand))
-    		rand = Math.floor(Math.random() * Math.floor(50)); 
-    	numsUsed.push(rand);
-        domain += alphabet[i] + "=" + rand;
-        if(i != alphabet.length-1){
-            domain += "&";
-        }
-    }
+    var domain = "https://bhuby.github.io/mvbingo/index.html?seed=" + j;
+
+    //var domain = "https://github.com/Bhuby/mvbingo/";
+    // for(var i=0; i<alphabet.length; i++){
+    // 	//console.log(numsUsed.includes(rand));
+
+    // 	while(numsUsed.includes(rand))
+    // 		rand = Math.floor(Math.random() * Math.floor(50)); 
+    // 	numsUsed.push(rand);
+    //     domain += i;
+    //     if(i != alphabet.length-1){
+    //         domain += "&";
+    //     }
+    // }
+
     console.log(domain);
     numsUsed = [];
 }
