e8beaf2e2cb1eaf91d29153627a805613990fabe
Bhuby <55616111+Bhuby@users.noreply.github.com>
2020-09-22 11:18:11 -0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,12 +1,19 @@
 var alphabet = "abcdefghijklnopqrstuvwxy";
+var numsUsed = [];
+var rand = 0;
 for(var j=0; j<200; j++){
     //var domain = "https://bhuby.github.io/mvbingo/index.html?";
     var domain = "file:///Users/bhuvanb.balagar/bingo/index.html?";
     for(var i=0; i<alphabet.length; i++){
-        domain += alphabet[i]+"="+Math.floor(Math.random() * Math.floor(25));
+    	//console.log(numsUsed.includes(rand));
+    	while(numsUsed.includes(rand))
+    		rand = Math.floor(Math.random() * Math.floor(50)); 
+    	numsUsed.push(rand);
+        domain += alphabet[i] + "=" + rand;
         if(i != alphabet.length-1){
             domain += "&";
         }
     }
     console.log(domain);
+    numsUsed = [];
 }
